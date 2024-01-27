2f6a10d039428af8b02c14a5c9d259ab18b71cdc
21120455 <hoai0455fit@gmail.com>
2024-01-27 12:17:06 +0700

    update register

--- controllers/account.c.js
+++ controllers/account.c.js
@@ -3,10 +3,10 @@ const accountModel = require('../models/account.m');
 const bankModel = require('../models/bank.m');
 exports.profile = async (req, res, next) => {
     try {
-      
+
         const user = await accountModel.getByUserName(req.session.user.username);
         const bank = await bankModel.getById(user._id);
-        res.render("profile",{isLogin: true, title: "Profile", username: user.username,balance: bank.balance.toLocaleString('en-US')});
+        res.render("profile", { isLogin: true, title: "Profile", username: user.username, balance: bank.balance.toLocaleString('en-US') });
     } catch (error) {
         next(error);
     }
@@ -14,10 +14,11 @@ exports.profile = async (req, res, next) => {
 
 exports.register = async (req, res, next) => {
     try {
-        const {username }=req.body;
-        const user =await accountModel.getByUserName(username)
-        //create bank account
-        const data = await bankModel.insertOne(user._id);
+        const { username } = req.body;
+        const user = await accountModel.getByUserName(username)
+        if (!user) {//create bank account
+            const data = await bankModel.insertOne(user._id);
+        }
         return res.json("success");
     } catch (error) {
         next(error);
@@ -32,7 +33,7 @@ exports.getLogin = async (req, res, next) => {
 };
 exports.getTranfer = async (req, res, next) => {
     try {
-        res.render("tranfer", {isLogin:true, title: "Tranfer" });
+        res.render("tranfer", { isLogin: true, title: "Tranfer" });
     } catch (error) {
         next(error);
     }
@@ -42,9 +43,9 @@ exports.login = async (req, res, next) => {
     try {
         const { username, password } = req.body;
         const user = await accountModel.getByUserName(username)
-        req.session.user=user;
-        req.session.isAuthenticated=true;
-        req.session.cookie.maxAge = 10*60*60*1000;
+        req.session.user = user;
+        req.session.isAuthenticated = true;
+        req.session.cookie.maxAge = 10 * 60 * 60 * 1000;
         if (user != undefined) {
             const check = bcrypt.compareSync(password, user.password)
             if (check) {
